25fec07e2802a4308abb5394c20940fc827928ed
Yağız Can Aslan <yagizcanaslan0@gmail.com>
2021-12-07 18:14:28 +0300

    Final version with corrected outputs

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,5 @@
+// Yağız Can Aslan 22001943 CS201-3
+
 #include <iostream>
 #include <ctime>
 using namespace std;
@@ -10,17 +12,16 @@ int frequencyTable( int*, int, int*, int);
 
 int main() {
     // Parameters
-    int n = 100000;
-    int m = 1000;
+    int n = 750000;
+    int m = 5000;
 
     // Program Code
-
     for ( int i = 0; i < 2; i++ ) {
         for ( int j = 0; j < 10; j++ ) {
             conductExperiment( (j + 1) * n, m);
         }
 
-        m = 10 * m;
+        m = 2 * m;
     }
 
     return 0;
@@ -108,7 +109,7 @@ int linearSearch( int* array1, int array1Size, int* array2, int array2Size ) {
 }
 
 void conductExperiment( int nInput, int mInput) {
-    // ARRAY RANGES ARE [0, nInput);
+    // ARRAY RANGES ARE [0, 10^6);
     // Time Variable(s)
     clock_t startTime;
     double duration;
@@ -117,36 +118,19 @@ void conductExperiment( int nInput, int mInput) {
     // Parameters
     int n = nInput;
     int m = mInput;
-    //int range = 50; // range = from 0 to n/range exclusive (for range = 50, range of numbers = [0, 1999])
 
     // Arrays
     int* arr1 = new int[n];
     int* arr2 = new int[m];
 
     // Setting up the arrays
-    /*
-    for ( int i = 0; i < (n / range); i++ ) {
-        for ( int j = (i * range); j < ((i + 1) * range); j++ ) {
-            arr1[j] = i;
-        }
-    }
-    */
-
     for ( int i = 0; i < n; i++ ) {
         arr1[i] = i;
     }
-    
-    /*for ( int i = 0; i < n; i++ ) { // For array validation
-        cout << i << ": " << arr1[i] << endl;
-    }*/
 
     for ( int i = 0; i < m; i++ ) {
         arr2[i] = arr1[(n - 1) - m + i];
     }
-
-    /*for ( int i = 0; i < m; i++ ) { // For array validation
-        cout << i << ": " << arr2[i] << endl;
-    }*/
     // Setting up the arrays complete
 
     // Conducting the experiment
